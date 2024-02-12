dcaaadf7ff6bd47af8a53a1a7078c128e953c5df
sametdemiralay <demiralayahmetsamet@gmail.com>
2024-02-12 21:08:36 +0300

    updated card design

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
+import TicketCard from "./(components)/TicketCard";
+
 const Dashboard = () => {
   return (
-    <div>HELLO {process.env.MONGODB_URI}</div>
-  )
-}
+    <div className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+      <h1>HELLO {process.env.MONGODB_URI}</h1>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
